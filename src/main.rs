75ef32641403da14185aaac82fa15cd689086d6e
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-10-10 18:23:06 -0700

    refactor: move main code out of main function and into separate function

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,17 @@ mod constants;
 mod utils;
 mod write;
 
-use crate::{
-    cli::Cli,
-    utils::get_contiguous,
-    write::{build_writer, write_records},
-};
+use crate::{cli::Cli, write::build_writer};
 use anyhow::Result;
 use clap::Parser;
 use constants::{CellBarcodes, LigationBarcodes};
 use fxread::initialize_reader;
+use utils::run_matching;
 
 fn main() -> Result<()> {
     let cli = Cli::parse();
 
+    // Initialize barcode references.
     let liga_bc_ref = LigationBarcodes::new();
     let cell_bc_ref = if cli.exact {
         CellBarcodes::new_exact()
@@ -23,39 +21,26 @@ fn main() -> Result<()> {
         CellBarcodes::new_oneoff()
     };
 
-    let r1_reader = initialize_reader(&cli.r1)?;
-    let r2_reader = initialize_reader(&cli.r2)?;
-
+    // Initialize output filenames.
     let r1_output_filename = format!("{}_R1.fastq.gz", &cli.prefix,);
-
     let r2_output_filename = format!("{}_R2.fastq.gz", &cli.prefix,);
 
+    // Initialize readers and writers.
+    let mut r1_reader = initialize_reader(&cli.r1)?;
+    let mut r2_reader = initialize_reader(&cli.r2)?;
     let mut r1_writer = build_writer(&r1_output_filename, cli.threads, cli.level)?;
-
     let mut r2_writer = build_writer(&r2_output_filename, cli.threads, cli.level)?;
 
-    let mut num_records = 0;
-    let mut matched_records_liga = 0;
-    let mut matched_records_cell = 0;
-
-    for (r1, r2) in r1_reader.zip(r2_reader) {
-        if let Some(liga) = liga_bc_ref.contains(&r1) {
-            let cell_bc = get_contiguous(&r1, liga.len() + cli.bc_offset, cli.bc_length);
-
-            if let Some(cell) = cell_bc_ref.contains(cell_bc) {
-                let umi = get_contiguous(&r1, liga.len() + cli.umi_offset, cli.umi_length);
-
-                write_records(&mut r1_writer, &mut r2_writer, cell, umi, &r1, &r2)?;
-                matched_records_cell += 1;
-            }
-            matched_records_liga += 1;
-        }
-        num_records += 1;
-    }
-
-    eprintln!("Total records: {}", num_records);
-    eprintln!("Ligation matched: {}", matched_records_liga);
-    eprintln!("Cell matched: {}", matched_records_cell);
+    // Match barcodes and write to output files.
+    run_matching(
+        &mut r1_reader,
+        &mut r2_reader,
+        &mut r1_writer,
+        &mut r2_writer,
+        &liga_bc_ref,
+        &cell_bc_ref,
+        &cli,
+    )?;
 
     Ok(())
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,13 @@
+use std::io::Write;
+
+use anyhow::Result;
 use fxread::Record;
+
+use crate::{
+    cli::Cli,
+    constants::{CellBarcodes, LigationBarcodes},
+    write::write_records,
+};
 
 /// Recover a continuous stretch of bytes from a record.
 pub fn get_contiguous<'a>(record: &'a Record, offset: usize, length: usize) -> &[u8] {
@@ -8,4 +17,44 @@ pub fn get_contiguous<'a>(record: &'a Record, offset: usize, length: usize) -> &
         panic!("Requested sequence is out of bounds");
     }
     &seq[offset..end]
+}
+
+/// Perform matching of barcodes and write to output files.
+pub fn run_matching<R, W>(
+    r1_reader: &mut R,
+    r2_reader: &mut R,
+    r1_writer: &mut W,
+    r2_writer: &mut W,
+    liga_bc_ref: &LigationBarcodes,
+    cell_bc_ref: &CellBarcodes,
+    cli: &Cli,
+) -> Result<()>
+where
+    R: Iterator<Item = Record>,
+    W: Write,
+{
+    let mut num_records = 0;
+    let mut matched_records_liga = 0;
+    let mut matched_records_cell = 0;
+
+    for (r1, r2) in r1_reader.zip(r2_reader) {
+        if let Some(liga) = liga_bc_ref.contains(&r1) {
+            let cell_bc = get_contiguous(&r1, liga.len() + cli.bc_offset, cli.bc_length);
+
+            if let Some(cell) = cell_bc_ref.contains(cell_bc) {
+                let umi = get_contiguous(&r1, liga.len() + cli.umi_offset, cli.umi_length);
+
+                write_records(r1_writer, r2_writer, cell, umi, &r1, &r2)?;
+                matched_records_cell += 1;
+            }
+            matched_records_liga += 1;
+        }
+        num_records += 1;
+    }
+
+    eprintln!("Total records: {}", num_records);
+    eprintln!("Ligation matched: {}", matched_records_liga);
+    eprintln!("Cell matched: {}", matched_records_cell);
+
+    Ok(())
 }
