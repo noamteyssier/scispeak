4020d8c7705e6068f9f62968cd72b3f470cac546
noam teyssier <22600644+noamteyssier@users.noreply.github.com>
2023-10-10 18:36:31 -0700

    feat: added a spinner to update progress

--- src/utils.rs
+++ src/utils.rs
@@ -1,13 +1,17 @@
-use std::io::Write;
+use std::{io::Write, time::Instant};
 
 use anyhow::Result;
 use fxread::Record;
+use spinoff::{spinners, Color, Spinner, Streams};
 
 use crate::{
     cli::Cli,
     constants::{CellBarcodes, LigationBarcodes},
     write::write_records,
 };
+
+/// Update frequency for the spinner (ms).
+const UPDATE_FREQUENCY: usize = 1000;
 
 /// Recover a continuous stretch of bytes from a record.
 pub fn get_contiguous(record: &Record, offset: usize, length: usize) -> &[u8] {
@@ -37,6 +41,14 @@ where
     let mut matched_records_liga = 0;
     let mut matched_records_cell = 0;
 
+    let mut spinner = Spinner::new_with_stream(
+        spinners::Dots12,
+        format!("Converting records"),
+        Color::Green,
+        Streams::Stderr,
+    );
+    let mut time_start = Instant::now();
+
     for (r1, r2) in r1_reader.zip(r2_reader) {
         if let Some(liga) = liga_bc_ref.contains(&r1) {
             let cell_bc = get_contiguous(&r1, liga.len() + cli.bc_offset, cli.bc_length);
@@ -49,8 +61,15 @@ where
             }
             matched_records_liga += 1;
         }
+
+        let time_now = Instant::now();
+        if time_now.duration_since(time_start).as_millis() > UPDATE_FREQUENCY as u128 {
+            spinner.update_text(format!("Converting records: {}", num_records));
+            time_start = time_now;
+        }
         num_records += 1;
     }
+    spinner.success("Done!");
 
     eprintln!("Total records: {}", num_records);
     eprintln!("Ligation matched: {}", matched_records_liga);
